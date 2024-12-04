c554442af31136b76a32eb9028a56cd63800db00
izzyK <g00424523@atu.ie>
2024-12-04 01:59:38 +0000

    update create component with submission form & axios to handle submission

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const module = {title};
+        console.log(module);
+
+        axios.post('http://localhost:4000/api/modules',module)
+        .then((res)=>{console.log(res.data)})
+        .catch();
+    }
+
     return (
         <div>
-          <h1>Create Page</h1>
+            <h3>Create component!</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Module Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Add Module Data"></input>
+                </div>
+            </form>
         </div>
-      );
+    );
 }
-
-export default Create; // Export Create component
+export default Create;
