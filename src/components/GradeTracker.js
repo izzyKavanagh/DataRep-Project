48aecc9ffa32d561ed7df8e710b9d9ee0a347a60
izzyK <g00424523@atu.ie>
2024-12-04 00:55:17 +0000

    iterate and render ModuleItem for each module

---
+++ src/components/GradeTracker.js
@@ -0,0 +1,14 @@
+// Import MovieItem component 
+import ModuleItem from "./ModuleItem";
+
+//arrow 'Movies' function that receives props variable and passes content to MovieItem and displays it
+const Modules = (props) => {
+    return props.myModules.map( //iterate over every object in myMovies array using .map() function
+        (module)=>{
+            //each object in myMovies array is passed to MovieItem
+            return <ModuleItem mymodule={module} key={module.mID}/> //passes object and unique key (imdbID)
+        }
+    )
+  };
+  
+  export default Modules; //exports Movies function so it can be imported and used in other files
