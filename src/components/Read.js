f681cc2badf9f1fab9c7956f8562f8ff86eaa192
izzyK <g00424523@atu.ie>
2024-12-04 00:58:44 +0000

    pass module data to Modules component

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,9 +1,25 @@
+import Modules from "./GradeTracker";
+//reusable component 'Read'
+
+//arrow 'Read' function that returns json "movies" content when called 
 const Read = () => {
-    return (
-        <div>
-          <h1>Read Page</h1>
-        </div>
-      );
-}
+  //json content: array of movie objects with Title, Year, imdbID, Type, Poster
+  const data = [
+    {
+      "Title": "Module Title",
+      "mID": "g00"
+    }
+  ];
+
+  return ( //returns h3 message and displays content
+    <div>
+        <h3>Read component</h3>
+        {/* displays Movies component with 'data' array passed as 'myMovies' prop */}
+        <Modules myModules={data}/> 
+        {/* displays footer content */}
+    </div>
+  );
+    
+};
 
 export default Read; // Export Read component
