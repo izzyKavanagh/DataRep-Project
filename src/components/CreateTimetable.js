b8326b7e421d7411dba0607f2e300ed8d6df5a94
izzyK <g00424523@atu.ie>
2024-12-05 22:16:51 +0000

    add state to manage timetable entries

--- src/components/CreateTimetable.js
+++ src/components/CreateTimetable.js
@@ -2,7 +2,12 @@ import { useState } from "react";
 import axios from "axios";
 
 const Timetable = () => {
-  
+  // State to manage the timetable entries
+  const [subject, setSubject] = useState('');
+  const [date, setDate] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [timetable, setTimetable] = useState([]);
 }
 
 export default Timetable;
