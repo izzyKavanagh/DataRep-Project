cf6b7cf00801514939d20e386550722a069d384e
izzyK <g00424523@atu.ie>
2024-12-05 22:20:16 +0000

    handle form submission for new timetable

--- src/components/CreateTimetable.js
+++ src/components/CreateTimetable.js
@@ -4,10 +4,14 @@ import axios from "axios";
 const Timetable = () => {
   // State to manage the timetable entries
   const [subject, setSubject] = useState('');
-  const [date, setDate] = useState('');
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
   const [timetable, setTimetable] = useState([]);
+}
+
+const handleAddTimetable = (e) => {
+    e.preventDefault();
+    const newEntry = { subject, startTime, endTime };
 }
 
 export default Timetable;
