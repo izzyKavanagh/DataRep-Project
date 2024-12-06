6b205093d25b2bc25863b477d2ee81af20b084b5
izzyK <g00424523@atu.ie>
2024-12-06 14:43:32 +0000

    create component to display to-do list items

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import Card from 'react-bootstrap/Card';
+import { useEffect } from 'react';
 
-const ToDoList = () => {
-  const [todos, setTodos] = useState([]); // State to store the list of to-dos
+const TodoItem = (props) => {
+  useEffect(() => {
+    console.log("Task:", props.todo);
+  }, [props.todo]);
 
-  return (
-    <div>
-        {/* List of To-Dos */}
-        <ul className="list-group">
-            <p>No items yet. Add something to get started!</p>
-            <li className="list-group-item">
-            </li>
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            {/* Card to display the to-do item */}
+            <Card>
+                <Card.Body>
+                    <p>
+                        <strong>Task:</strong> {props.todo.task}
+                    </p>
+                    <p>
+                        <strong>Status:</strong> {props.todo.completed ? 'Completed' : 'Incomplete'}
+                    </p>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
 
-}
+export default TodoItem;
