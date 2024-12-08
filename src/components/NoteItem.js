187299a630f9466272c1dc4eefa427109f912dd0
izzyK <g00424523@atu.ie>
2024-12-08 01:24:47 +0000

    create components for notes pages

---
+++ src/components/NoteItem.js
@@ -0,0 +1,10 @@
+//import bootstrap components for card formating 
+import Card from 'react-bootstrap/Card';
+import { useEffect } from 'react';
+
+const NoteItem = (props) => {
+
+
+  };
+  
+  export default NoteItem; //exports MovieItem function so it can be imported and used in other files
---
+++ src/components/Notes.js
@@ -0,0 +1,7 @@
+import NoteItem from "./noteItem";
+
+const Notes = (props) => {
+
+};
+  
+  export default Notes; //exports Movies function so it can be imported and used in other files
