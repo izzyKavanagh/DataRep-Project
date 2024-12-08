ec6c1364b20d4eacec28abc599268a8616b1cf55
izzyK <g00424523@atu.ie>
2024-12-08 17:05:53 +0000

    update NoteItem so it displays all details of a note

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,6 +4,28 @@ import { useEffect } from 'react';
 
 const NoteItem = (props) => {
 
+    useEffect(() => {
+        console.log("Module Item:", props.mynote);
+      }, [props.mynote]); // Only run this effect when the mymovie prop changes
+
+    return (
+        <div>
+            {/* Bootstrap Card component to display movie information */}
+            <Card>
+                {/* Card Header displays movie title */}
+                <Card.Header>{props.mynote.title}</Card.Header>
+                {/* Card Body contains movie information */} 
+                <Card.Body>
+                    {/* Blockquote used to add movie poster and year */}
+                    <blockquote className="blockquote mb-0">
+                        <p>Date Created: {props.mynote.dateCreeated}</p>
+                        <p>Date Created: {props.mynote.dateEdited}</p>
+                        <p>Note Body: {props.mynote.noteBody}</p>
+                    </blockquote>
+                </Card.Body>
+           </Card>
+        </div>
+      );
 
   };
   
