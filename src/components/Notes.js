b4102d1a9aeda4042e0d28babdd4ab1de54aaf39
izzyK <g00424523@atu.ie>
2024-12-08 17:06:57 +0000

    update Notes.js so it renders notes by iterating over myNotes prop

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,12 @@
-import NoteItem from "./noteItem";
+import NoteItem from "./NoteItem";
 
 const Notes = (props) => {
-
+  return props.myNotes.map(
+    (note)=>{
+        //each object in myMovies array is passed to MovieItem
+        return <NoteItem mynote={note} key={note._id}/> //passes object and unique key (_id)
+    }
+)
 };
   
   export default Notes; //exports Movies function so it can be imported and used in other files
