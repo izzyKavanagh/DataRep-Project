c8c42adf073ac1de5282eac9bc47a4567400a005
izzyK <g00424523@atu.ie>
2024-12-08 17:18:00 +0000

    display note dates as day/month/year

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,28 +4,33 @@ import { useEffect } from 'react';
 
 const NoteItem = (props) => {
 
-    useEffect(() => {
-        console.log("Module Item:", props.mynote);
-      }, [props.mynote]); // Only run this effect when the mymovie prop changes
+  const formatDate = (date) => {
+    if (!date) return 'N/A'; // Handle missing dates
+      return new Date(date).toLocaleDateString('en-GB');
+  };
 
-    return (
-        <div>
-            {/* Bootstrap Card component to display movie information */}
-            <Card>
-                {/* Card Header displays movie title */}
-                <Card.Header>{props.mynote.title}</Card.Header>
-                {/* Card Body contains movie information */} 
-                <Card.Body>
-                    {/* Blockquote used to add movie poster and year */}
-                    <blockquote className="blockquote mb-0">
-                        <p>Date Created: {props.mynote.dateCreeated}</p>
-                        <p>Date Created: {props.mynote.dateEdited}</p>
-                        <p>Note Body: {props.mynote.noteBody}</p>
-                    </blockquote>
-                </Card.Body>
-           </Card>
-        </div>
-      );
+  useEffect(() => {
+      console.log("Module Item:", props.mynote);
+    }, [props.mynote]); // Only run this effect when the mymovie prop changes
+
+  return (
+      <div>
+          {/* Bootstrap Card component to display movie information */}
+          <Card>
+              {/* Card Header displays movie title */}
+              <Card.Header>{props.mynote.title}</Card.Header>
+              {/* Card Body contains movie information */} 
+              <Card.Body>
+                  {/* Blockquote used to add movie poster and year */}
+                  <blockquote className="blockquote mb-0">
+                      <p>Date Created: {formatDate(props.mynote.dateCreated)}</p>
+                      <p>Date Edited: {formatDate(props.mynote.dateEdited)}</p>
+                      <p>Note Body: {props.mynote.noteBody}</p>
+                  </blockquote>
+              </Card.Body>
+          </Card>
+      </div>
+    );
 
   };
   
