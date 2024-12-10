b94bdc2c760e04fa4bc05ff520d3f5d04ecb3daa
izzyK <g00424523@atu.ie>
2024-12-10 18:58:20 +0000

    add delete button to card footer

--- src/components/gradeCalcItem.js
+++ src/components/gradeCalcItem.js
@@ -10,15 +10,22 @@ const GradeItem = (props) => {
     return (
       <div style={{  width: "100%", padding: "10px" }}>
         <Card style={{ borderRadius: "20px"}}>
-          <Card.Header>{props.mygradecalcs.module}</Card.Header>
-          <Card.Body>
+            <Card.Header>{props.mygradecalcs.module}</Card.Header>
+            <Card.Body>
             <blockquote className="blockquote mb-0">
                 <p>Title: {props.mygradecalcs.title}</p>
                 <p>Grade: {props.mygradecalcs.grade}%</p>
                 <p>Weighting: {props.mygradecalcs.weighting}%</p>
                 <p>Result: {props.mygradecalcs.result}%</p>
             </blockquote>
-          </Card.Body>
+            </Card.Body>
+            <Card.Footer style={{ position: "relative" }}>
+                {/* Delete button in the bottom-right corner */}
+                <button style={{border: "1px solid black", borderRadius: "20px", cursor: "pointer", 
+                    fontSize: "12px", padding: "3px 8px", backgroundColor: "rgba(255, 0, 0, 0.6)"}}>
+                Delete
+                </button>
+            </Card.Footer>
         </Card>
       </div>
     );
