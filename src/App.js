da7d88278d43dc328eb8fd57e286120ecec8d35f
izzyK <g00424523@atu.ie>
2024-12-12 23:14:34 +0000

    update app.js after component deletion

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/navbar.js';
 import WelcomePage from './components/WelcomePage.js';
-import CreateModuleItem from './components/CreateModuleItem.js';
-import GradeTracker from './components/GradeTracker.js';
-import ReadTimetables from './components/ReadTimetable.js';
-import CreateTimetable from './components/CreateTimetable.js';
 import ReadToDoList from './components/ReadToDoList.js';
 import ReadNotes from './components/ReadNotes.js';
 import CreateNote from './components/CreateNote.js';
@@ -19,10 +15,6 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/home" element={<WelcomePage />} />
-        <Route path="/createModule" element={<CreateModuleItem />} />
-        <Route path="/read" element={<GradeTracker />} />
-        <Route path="/timetables" element={<ReadTimetables />} />
-        <Route path="/createTimetable" element={<CreateTimetable />} />
         <Route path="/todolist" element={<ReadToDoList />} />
         <Route path="/notes" element={<ReadNotes />} />
         <Route path="/createNote" element={<CreateNote />} />
